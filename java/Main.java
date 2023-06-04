00c958699926985739edcfc19ebd62684acdf66e
pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-06-05 00:10:22 +0530

    additon using operators

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Add.add(10,20));
+		System.out.println(Add.add(10.2,20.2));
+		System.out.println(Add.add(10L,20L));
+		System.out.println(Add.add((short)10,(short)20));
+		System.out.println(Add.add(10.2f,20.2f));
+		
+		
+	
+	}
+}
