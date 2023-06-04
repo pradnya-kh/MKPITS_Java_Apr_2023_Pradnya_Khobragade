07618739f6badf459ce2cc70fa0bf3f737c2e791
pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-06-05 00:30:41 +0530

    multiplication using operators

---
+++ java/MainMultiply.java
@@ -0,0 +1,23 @@
+class MainMultiply{
+	public static void main(String[] args){
+
+
+			Multiplication multiply1 = new Multiplication();
+			System.out.println(multiply1.multiplyInteger(20,40));
+
+	
+			Multiplication multiply2 = new Multiplication();
+			System.out.println(multiply2.multiplyFloat(80f,40f));
+
+
+			Multiplication multiply3 = new Multiplication();
+			System.out.println(multiply3.multiplyDouble(25.75,16.86));
+
+
+			Multiplication multiply4 = new Multiplication();
+			System.out.println(multiply4.multiplyLong(600,100));
+
+
+			
+	}
+}
---
+++ java/Multiplication.java
@@ -0,0 +1,22 @@
+ public class Multiplication{
+
+
+	public int multiplyInteger(int number1, int number2){
+		return number1 * number2;
+	}
+
+	public float multiplyFloat(float number1, float number2){
+		return number1 * number2;
+	}
+
+	public double multiplyDouble(double number1, double number2){
+		return number1 * number2;
+	}
+
+	public long multiplyLong(long number1, long number2){
+		return number1 * number2;
+	}
+
+	
+	
+}
