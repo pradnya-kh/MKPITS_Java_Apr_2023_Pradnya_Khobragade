d13e433769b81889ab17c2b43ca47e212e6aca34
pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-06-15 14:49:06 +0530

    inharitance collage example

---
+++ java/Collage.java
@@ -0,0 +1,18 @@
+Class Collage{
+double area;
+double width;
+double hight;
+}
+Class students extends collage{
+string name;
+string Id;
+string Dob;
+}
+Class stream extends students{
+list<Branch>Branches;
+list<Subject>Subjects;
+}
+Class Gender{
+string male;
+string female;
+}
