bbf2ffc16b60501c608e45b075b6e3ef7ddfa71a
pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-10-03 22:46:31 +0530

    join_query

---
+++ SQL_database/Join_query/que34.sql
@@ -0,0 +1,10 @@
+-- (34) Display the names of customers who have rented the film titled "Riders Zorro".
+     select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+
+ select first_name,last_name,email from customer join rental 
+ on rental.customer_id= customer.customer_id join inventory 
+ on  inventory.inventory_id=rental.inventory_id join film
+ on film.film_id=inventory.film_id where title="Riders Zorro"
