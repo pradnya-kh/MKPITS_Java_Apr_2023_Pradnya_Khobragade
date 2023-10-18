7e832614b1a7f5fd07e94e8a3174e7e575b81ed1
pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-10-18 18:21:38 +0530

    Update Marks

---
+++ SQL_database/Trigger/update_marks.sql
@@ -0,0 +1,30 @@
+use sakila
+drop table Marksdetails
+create table Marksdetails(ROLL_no int,physic int,chemistry int,maths int)
+select * from Marksdetails
+
+ 
+create table Total_details(Roll_no int,Total int)
+select * from Total_details
+ drop table Total_details
+
+delimiter //
+create trigger update_markss
+after
+update on Marksdetails
+for each row
+begin
+insert into Total_details   values (New.Roll_no, New.Physic+New.Chemistry+New.Maths);
+end //
+delimiter ;
+
+select * from  Marksdetails
+insert into  Marksdetails value(102,35,35,35);
+insert into  marksdetails value(103,45,45,45);
+insert into  marksdetails value(104,50,50,50);
+insert into marksdetails value (105,80,80,80);
+
+
+select * from Total_details
+
+drop trigger insert_marks
