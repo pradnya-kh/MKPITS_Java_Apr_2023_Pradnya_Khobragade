88094e95ae44b8513848f5ae2eb3c5d890bc81e5
Pradnya khobragade <pradnyakhobragade21@gmail.com>
2023-12-11 22:32:49 +0530

    programs on configrations used

---
+++ Springboot/configration_program/src/test/java/com/example/PreAndPostDistroyed/PreAndPostDistroyedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PreAndPostDistroyed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAndPostDistroyedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
